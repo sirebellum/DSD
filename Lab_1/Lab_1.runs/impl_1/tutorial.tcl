2d76563486309516edeccfd4f75e25aaa64b69d6
Lubuntu-VM <bluedude227@gmail.com>
2017-09-04 17:01:18 -0500

    Lab_1 Complete

--- Lab_1/Lab_1.runs/impl_1/tutorial.tcl
+++ Lab_1/Lab_1.runs/impl_1/tutorial.tcl
@@ -43,88 +43,23 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
+start_step write_bitstream
+set ACTIVE_STEP write_bitstream
 set rc [catch {
-  create_msg_db init_design.pb
-  create_project -in_memory -part xc7a100tcsg324-1
-  set_property board_part digilentinc.com:nexys4_ddr:part0:1.1 [current_project]
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
+  create_msg_db write_bitstream.pb
+  open_checkpoint tutorial_routed.dcp
   set_property webtalk.parent_dir /home/lubuntu/Dev/DSD/Lab_1/Lab_1.cache/wt [current_project]
-  set_property parent.project_path /home/lubuntu/Dev/DSD/Lab_1/Lab_1.xpr [current_project]
-  set_property ip_output_repo /home/lubuntu/Dev/DSD/Lab_1/Lab_1.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet /home/lubuntu/Dev/DSD/Lab_1/Lab_1.runs/synth_1/tutorial.dcp
-  read_xdc /home/lubuntu/Dev/DSD/Lab_1/Lab_1.srcs/constrs_1/imports/Downloads/Tutorial_C.xdc
-  link_design -top tutorial -part xc7a100tcsg324-1
-  close_msg_db -file init_design.pb
+  catch { write_mem_info -force tutorial.mmi }
+  write_bitstream -force tutorial.bit 
+  catch {write_debug_probes -no_partial_ltxfile -quiet -force debug_nets}
+  catch {file copy -force debug_nets.ltx tutorial.ltx}
+  close_msg_db -file write_bitstream.pb
 } RESULT]
 if {$rc} {
-  step_failed init_design
+  step_failed write_bitstream
   return -code error $RESULT
 } else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force tutorial_opt.dcp
-  catch { report_drc -file tutorial_drc_opted.rpt }
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  implement_debug_core 
-  place_design 
-  write_checkpoint -force tutorial_placed.dcp
-  catch { report_io -file tutorial_io_placed.rpt }
-  catch { report_utilization -file tutorial_utilization_placed.rpt -pb tutorial_utilization_placed.pb }
-  catch { report_control_sets -verbose -file tutorial_control_sets_placed.rpt }
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force tutorial_routed.dcp
-  catch { report_drc -file tutorial_drc_routed.rpt -pb tutorial_drc_routed.pb -rpx tutorial_drc_routed.rpx }
-  catch { report_methodology -file tutorial_methodology_drc_routed.rpt -rpx tutorial_methodology_drc_routed.rpx }
-  catch { report_power -file tutorial_power_routed.rpt -pb tutorial_power_summary_routed.pb -rpx tutorial_power_routed.rpx }
-  catch { report_route_status -file tutorial_route_status.rpt -pb tutorial_route_status.pb }
-  catch { report_clock_utilization -file tutorial_clock_utilization_routed.rpt }
-  catch { report_timing_summary -warn_on_violation -max_paths 10 -file tutorial_timing_summary_routed.rpt -rpx tutorial_timing_summary_routed.rpx }
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force tutorial_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
+  end_step write_bitstream
   unset ACTIVE_STEP 
 }
 
